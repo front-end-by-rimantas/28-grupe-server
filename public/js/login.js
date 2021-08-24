cc64675ac8b3d028a711952c908c0ff8bb68431b
Rimantas <belauzas@yahoo.com>
2021-08-25 00:44:05 +0300

    Display login response error

--- public/js/login.js
+++ public/js/login.js
@@ -48,6 +48,11 @@ function submitFormInfo(e) {
 function responseAction(response) {
     try {
         const responseObject = JSON.parse(response);
+        if (responseObject.error) {
+            localStorage.removeItem('login-token');
+            document.cookie = 'login-token=';
+            showMessage('error', responseObject.error);
+        }
         localStorage.setItem('login-token', responseObject.token);
         document.cookie = 'login-token=' + responseObject.token;
         showMessage('success', 'Token gautas ;)');
