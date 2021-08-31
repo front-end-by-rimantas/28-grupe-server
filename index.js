e6aca4b824de6031e7ffaee19025b0a5a3fd23c3
Rimantas <belauzas@yahoo.com>
2021-08-31 20:09:23 +0300

    MySQL

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+const mysql = require('mysql2/promise');
 const server = require('./lib/server');
 
 const app = {}
 
-app.init = () => {
+app.init = async () => {
+    // prisijungti prie duomenu bazes
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        database: 'barsukas',
+    });
+
     // paruosti reikiamus direktorijas
 
     // paruosti reikiamus failus
 
     // inicijuojame serveri
-    server.init();
+    server.init(connection);
 }
 
 app.init();
